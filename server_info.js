86b94551faf73a88ee9b46a3c697557e20ef01d6
Geert Weening <geertweening@gmail.com>
2014-11-10 09:55:07 -0800

    add server_info example

---
+++ server_info.js
@@ -0,0 +1,19 @@
+//
+// Requesting info from a rippled
+//
+
+/* Loading ripple-lib with Node.js */
+var Remote = require('ripple-lib').Remote;
+
+var remote = new Remote({
+  // see the API Reference for available options
+  servers: [ 'wss://s1.ripple.com:443' ]
+});
+
+remote.connect(function() {
+  /* remote connected */
+  remote.requestServerInfo(function(err, info) {
+    console.log(err, info);
+    process.exit(err === void(0));
+  });
+});
