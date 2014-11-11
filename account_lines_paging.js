09b2ce17a11b0e4e84218655c6c7f3da036a9581
Geert Weening <geertweening@gmail.com>
2014-11-11 15:43:37 -0800

    update account lines for rippled changes

--- account_lines_paging.js
+++ account_lines_paging.js
@@ -1,7 +1,7 @@
 //
 // Example on how to request account lines and page through the responses
 //
-// requires ripple-lib 0.9.2-rc4 or higher
+// requires ripple-lib 0.9.2-rc4 or higher and rippled-0.26.4-sp2 or higher
 //
 
 var Remote = require('ripple-lib').Remote;
@@ -28,7 +28,7 @@ remote.connect(function() {
 remote.once('ledger_closed', function(ledger) {
   // we need to provide a ledger_index or ledger_hash to make sure we
   // get a complete and reliable result for all account lines
-  getAccountLines(rippleAccount, getRandomLimit(), ledger.ledger_index);
+  getAccountLines(rippleAccount, getRandomLimit(), 'validated');
 });
 
 function getAccountLines(rippleAccount, limit, ledger_index, marker) {
@@ -49,7 +49,12 @@ function getAccountLines(rippleAccount, limit, ledger_index, marker) {
   });
 
   request.once('success', function(result) {
+    // console.log(JSON.stringify(result, null, 2));
     console.log('received ' + result.lines.length + ' lines, for limit of ' + limit);
+    console.log('ledger_current_index: ' + result.ledger_current_index);
+    console.log('ledger_index: ' + result.ledger_index);
+    console.log('ledger_hash: ' + result.ledger_hash);
+    console.log('validated: ' + result.validated);
     console.log('marker: ' + result.marker);
 
     result.lines.forEach(function(line) {
@@ -62,6 +67,8 @@ function getAccountLines(rippleAccount, limit, ledger_index, marker) {
 
     // if there's a marker, keep on querying
     if (result.marker) {
+      ledger_index = result.ledger_hash || result.ledger_index || result.ledger_current_index || ledger_index;
+      console.log('used ledger index', ledger_index);
       getAccountLines(rippleAccount, getRandomLimit(), ledger_index, result.marker);
     } else {
       done();
